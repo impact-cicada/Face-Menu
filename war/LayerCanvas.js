063a1c6e126edc0bbeb447df2ffb2e428cc2b33e
cicada <sasaleaf01@gmail.com>
2018-06-07 01:10:04 +0900

    rfct:canvas全体の変数や処理をCanvasManagerに定義

--- war/LayerCanvas.js
+++ war/LayerCanvas.js
@@ -2,91 +2,99 @@
 var Layer = function(src, order, cvX, cvY, cvW, cvH){
 	this.image = new Image();
 	this.image.src = src;
-	this.image.addEventListener('load', function(){
-			if(loadedCount == layers.length){
-				drawLayers();
-			}
-			loadedCount++;
-		}, false);
+	this.image.addEventListener('load', onLoad, false);
 	this.order = order;
 	this.cvX = cvX;
 	this.cvY = cvY;
 	this.cvW = cvW;
 	this.cvH = cvH;
 };
+
+function onLoad(){
+	if(cvMgr.loadedCount == cvMgr.layers.length){
+		cvMgr.drawLayers();
+	}
+	cvMgr.loadedCount++;
+};
+
 // 画像を描画
 Layer.prototype.draw = function() {
 	console.log('draw() ' + this.image.src + ' x:'+this.cvX + ' y:'+this.cvY + ' w:'+this.cvW + ' h:'+this.cvH);
-	context.drawImage(this.image, this.cvX, this.cvY, this.cvW, this.cvH);
+	cvMgr.context.drawImage(this.image, this.cvX, this.cvY, this.cvW, this.cvH);
 };
+
 // 指定座標が画像描画領域内にあるか判定
 //  true :領域内
 //  false:領域外
 Layer.prototype.has = function(tgtX, tgtY){
 	return (this.cvX < tgtX && tgtX < (this.cvX + this.cvW) && this.cvY < tgtY && tgtY < (this.cvY + this.cvH))
-}
-
-//// --------------- 全レイヤー描画 --------------- ////
-var drawLayers = function(){
-	for(var i in layers){
-		layers[i].draw();
-	}
 };
 
-var loadedCount = 1;
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-var layers = [];
-var dragLayer = -1;
-var relX, relY;
+//// --------------- CanvasManager Class --------------- ////
+var CanvasManager = function(){
+	this.loadedCount = 1;
+	this.canvas = document.getElementById('canvas');
+	this.context = this.canvas.getContext('2d');
+	this.layers = [];
+	this.dragLayer = -1;
+	this.relX = 0;
+	this.relY = 0;
 
-canvas.addEventListener('mousedown', onDown, false);
-canvas.addEventListener('mousemove', onMove, false);
-canvas.addEventListener('mouseup', onUp, false);
+	this.canvas.addEventListener('mousedown', onDown, false);
+	this.canvas.addEventListener('mousemove', onMove, false);
+	this.canvas.addEventListener('mouseup', onUp, false);
+};
+
+//// --------------- 全レイヤー描画 --------------- ////
+CanvasManager.prototype.drawLayers = function(){
+	for(var i in this.layers){
+		this.layers[i].draw();
+	}
+};
 
 //// --------------- マウスダウン --------------- ////
 function onDown(e) {
 	// キャンバスの左上端の座標を取得
-	var offsetX = canvas.getBoundingClientRect().left;
-	var offsetY = canvas.getBoundingClientRect().top;
+	var offsetX = cvMgr.canvas.getBoundingClientRect().left;
+	var offsetY = cvMgr.canvas.getBoundingClientRect().top;
 
 	// マウスが押された座標を取得
 	var tgtX = e.clientX - offsetX;
 	var tgtY = e.clientY - offsetY;
 
 	// 画像上の座標かどうかを判定
-	for(var i = (layers.length - 1); 0 <= i; i--){
-		if(layers[i].has(tgtX, tgtY)){
-			dragLayer = i; // ドラッグ開始
-			relX = layers[i].cvX - tgtX;
-			relY = layers[i].cvY - tgtY;
+	for(var i = (cvMgr.layers.length - 1); 0 <= i; i--){
+		if(cvMgr.layers[i].has(tgtX, tgtY)){
+			cvMgr.dragLayer = i; // ドラッグ開始
+			cvMgr.relX = cvMgr.layers[i].cvX - tgtX;
+			cvMgr.relY = cvMgr.layers[i].cvY - tgtY;
 			break;
 		}
 	}
-}
+};
 
 //// --------------- マウス移動 --------------- ////
-function onMove(e) {
+function onMove (e) {
 	// キャンバスの左上端の座標を取得
-	var offsetX = canvas.getBoundingClientRect().left;
-	var offsetY = canvas.getBoundingClientRect().top;
+	var offsetX = cvMgr.canvas.getBoundingClientRect().left;
+	var offsetY = cvMgr.canvas.getBoundingClientRect().top;
 
 	// マウスが移動した先の座標を取得
 	var tgtX = e.clientX - offsetX;
 	var tgtY = e.clientY - offsetY;
 
 	// ドラッグが開始されていればオブジェクトの座標を更新して再描画
-	if(0 <= dragLayer){
-		layers[dragLayer].cvX = tgtX + relX;
-		layers[dragLayer].cvY = tgtY + relY;
-		drawRect();
+	if(0 <= cvMgr.dragLayer){
+		cvMgr.layers[cvMgr.dragLayer].cvX = tgtX + cvMgr.relX;
+		cvMgr.layers[cvMgr.dragLayer].cvY = tgtY + cvMgr.relY;
+		cvMgr.drawRect();
 	}
 	// ドラッグ中以外
 	else{
 		// 画像上の座標かどうかを判定
 		var onImg = false;
-		for(var i = (layers.length - 1); 0 <= i; i--){
-			if(layers[i].has(tgtX, tgtY)){
+		for(var i = (cvMgr.layers.length - 1); 0 <= i; i--){
+			if(cvMgr.layers[i].has(tgtX, tgtY)){
 				onImg = true;
 				break;
 			}
@@ -97,15 +105,15 @@ function onMove(e) {
 			document.body.style.cursor='auto';
 		}
 	}
-}
+};
 
 //// --------------- マウスアップ --------------- ////
 function onUp(e) {
-	dragLayer = -1; // ドラッグ終了
-}
+	cvMgr.dragLayer = -1; // ドラッグ終了
+};
 
 //// --------------- 再描画 --------------- ////
-function drawRect() {
-	context.clearRect(0, 0, canvas.width, canvas.height); // キャンバスをクリア
-	drawLayers();
-}
+CanvasManager.prototype.drawRect = function() {
+	this.context.clearRect(0, 0, this.canvas.width, this.canvas.height); // キャンバスをクリア
+	this.drawLayers();
+};
